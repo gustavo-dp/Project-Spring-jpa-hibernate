16b2e31c0c09d8f14502d84d104c87ce5a3a8410
Gustavo Deda <gustavo.deda687@gmail.com>
2025-06-17 10:29:39 -0300

    First commit

---
+++ src/test/java/com/educandoweb/courseSpring/CourseSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.courseSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
